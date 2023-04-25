d13d42257fd3ba8eff2015538ff931590ed42194
jamersonsouza <jamersonsouza@outlook.com>
2023-04-24 22:17:41 -0300

    Serviço de autenticação jwt

---
+++ src/app/service/authservice.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { API_URL } from '../config/api.config';
+import { Credenciais } from '../models/credenciais';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthserviceService {
+
+  jwtService : JwtHelperService = new JwtHelperService();
+
+  constructor(private http : HttpClient) { }
+
+  authenticate(credenciais : Credenciais){
+    return this.http.post(`${API_URL.baseURL}/login`, credenciais, {
+        observe: 'response',
+        responseType: 'text'
+    });
+  }
+
+
+}
