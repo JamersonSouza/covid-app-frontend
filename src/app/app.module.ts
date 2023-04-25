1c09db16b5684442d418e209f9e53483858df295
jamersonsouza <jamersonsouza@outlook.com>
2023-04-24 21:44:47 -0300

    definição da rota e imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModuleModule } from './shared/shared-module/shared-module.module';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    InputTextModule,
+    BrowserAnimationsModule,
+    SharedModuleModule,
     ButtonModule,
-    InputTextModule
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
