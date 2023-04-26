7c001d13d01b4781628d770cd7e8b128e71f4693
jamersonsouza <jamersonsouza@outlook.com>
2023-04-26 10:01:26 -0300

    interfaces para os dados da api

---
+++ src/app/models/country.ts
@@ -0,0 +1,16 @@
+export interface Country {
+
+  Country: string;
+  CountryCode: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+  Date: string;
+  ID: string;
+  Slug: string;
+
+
+}
