8efebd8231f09129ad77a6207c1ebfb944a40166
jamersonsouza <jamersonsouza@outlook.com>
2023-04-25 22:10:13 -0300

    Component index que é exibido quando o usuário loga

---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CovidService } from 'src/app/service/covid.service';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent {
+
+
+  data : any;
+
+  constructor(private covidService : CovidService){}
+
+  ngOnInit(): void {
+    this.listCasesGlobal();
+  }
+
+  listCasesGlobal():void{
+    this.covidService.casesGlobal().subscribe(res => {
+      this.data = res;
+      console.log(this.data);
+    })
+  }
+
+}
