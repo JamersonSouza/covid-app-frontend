83884167621134283314a872ff0ed9e65a641f7e
jamersonsouza <jamersonsouza@outlook.com>
2023-04-26 22:01:23 -0300

    Guards adicionado ao projeto para proteção de rotas

---
+++ src/app/auth/guard.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthserviceService } from '../service/authservice.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+
+  constructor(private authService : AuthserviceService, private route: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      let authenticate = this.authService.isAuthenticated();
+      if(authenticate){
+        return true;
+      }else{
+        this.route.navigate(['']);
+        return false;
+      }
+
+  }
+
+}
